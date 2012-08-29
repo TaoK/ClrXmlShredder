4d33c984138566cb04f1502f7949f85be3602eb4
TaoK <tao@klerks.biz>
2012-08-29 12:18:44 -0400

    Initial testing framework

---
+++ ClrXmlShredderTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClrXmlShredderTests
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main(string[] args)
+        {
+            NUnit.Gui.AppEntry.Main(new string[] { System.Reflection.Assembly.GetExecutingAssembly().Location });
+        }
+    }
+}
---
+++ ClrXmlShredderTests/ShreddingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace ClrXmlShredderTests
+{
+    [TestFixture]
+    class ShreddingTests
+    {
+        [TestCase]
+        public void DoSomething()
+        {
+            ConnectionStringSettingsCollection strings = ConfigurationManager.ConnectionStrings;
+            ConnectionStringSettings thisString = null;
+            if (strings != null)
+                thisString = strings["LocalSql"];
+
+            if (thisString != null)
+            {
+                using (SqlConnection myConnection = new SqlConnection(thisString.ConnectionString))
+                using (SqlCommand testcomm = new SqlCommand("SELECT 1", myConnection))
+                {
+                    myConnection.Open();
+                    testcomm.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
